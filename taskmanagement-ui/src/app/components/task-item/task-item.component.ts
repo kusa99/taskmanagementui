9804ec3ea280f9af84f1e755e444938beff79e73
Harun Kusić <harun.kusic2018@size.ba>
2021-04-09 23:05:39 +0200

    add-task style

--- taskmanagement-ui/src/app/components/task-item/task-item.component.ts
+++ taskmanagement-ui/src/app/components/task-item/task-item.component.ts
@@ -13,7 +13,4 @@ export class TaskItemComponent implements OnInit {
   ngOnInit(): void {}
 
 
-  onCreate(){
-    
-  }
 }
--- taskmanagement-ui/src/app/components/tasks/tasks.component.ts
+++ taskmanagement-ui/src/app/components/tasks/tasks.component.ts
@@ -65,4 +65,7 @@ export class TasksComponent implements OnInit {
       },
     ];
   }
+  onCreate(){
+    
+  }
 }
